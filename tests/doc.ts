a83b3602ac7e70eddeb26b347dd3fe7df6f13af1
Luthaf <luthaf@luthaf.fr>
2020-03-04 15:52:04 +0100

    Setup doc + doctests

---
+++ tests/doc.ts
@@ -0,0 +1,31 @@
+import {assert}  from 'chai';
+import * as vm from 'vm';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as glob from 'glob';
+
+import {ready} from '../src/index';
+import * as chemfiles from '../src/index';
+
+// run dynamically generated doctest files
+describe('Doctests', () => {
+    before((done) => {ready(() => done());});
+
+    for (const file of glob.sync(path.join(__dirname, 'doc', '**/*.ts'))) {
+        const context = {
+            test: it,
+            assert: assert,
+            chemfiles: chemfiles,
+        };
+
+        const basename = path.basename(file);
+        const options = {
+            filename: basename.split('.').slice(0, 2).join('.'),
+            // -4 since that the number of lines added before the actual test
+            lineOffset: parseInt(basename.split('-')[1].split('.')[0]) - 4,
+        };
+
+        const code = fs.readFileSync(file, {encoding: "utf8"});
+        vm.runInNewContext(code, context, options);
+    }
+});
